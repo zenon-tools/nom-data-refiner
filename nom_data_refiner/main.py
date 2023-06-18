fcfb2dee3a9cbf8dd90a11c9db07d0aa6d09fb73
vilkris <vilkris@protonmail.com>
2023-06-18 11:49:54 +0300

    Update ZNN CoinGecko API ID and set QSR price to 1/10th of ZNN price

--- nom_data_refiner/main.py
+++ nom_data_refiner/main.py
@@ -145,8 +145,8 @@ async def update():
 
     # Get coin prices.
     market = MarketWrapper()
-    znn_price = await market.get_price_usd(coin='zenon')
-    qsr_price = await market.get_price_usd(coin='quasar')
+    znn_price = await market.get_price_usd(coin='zenon-2')
+    qsr_price = round(znn_price / 10, 2)
     bnb_price = await market.get_price_usd(coin='binancecoin')
 
     # If bad response use cached price data, else cache the new data.
